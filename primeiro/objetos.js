b32b906ad39a003c7d8fa0df02b87f810bcd3dfc
unknown <vitorjao56@gmail.com>
2020-06-16 10:20:47 -0300

    desafio objetos

---
+++ primeiro/objetos.js
@@ -0,0 +1,12 @@
+const empresa = {
+  nome: "Rocketseat",
+  cor: "roxo",
+  endereco: {
+    rua: "Rua Guilherme Gembala",
+    numero: 260,
+  },
+};
+
+console.log(
+  `A empresa ${empresa.nome} está localizada em ${empresa.endereco.rua} número ${empresa.endereco.numero}`
+);
